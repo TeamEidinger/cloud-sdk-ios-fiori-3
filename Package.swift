1f6050f10dd5bb7a1a7223debfe09ec8694b4cbb
Marco Eidinger <eidingermarco@gmail.com>
2023-04-17 13:16:16 -0700

    simplify

--- Package.swift
+++ Package.swift
@@ -19,30 +19,16 @@ let package = Package(
             targets: ["FioriCharts"]
         ),
         .library(
-            name: "FioriIntegrationCards",
-            type: .dynamic,
-            targets: ["FioriIntegrationCards"]
-        ),
-        .library(
             name: "FioriThemeManager",
             type: .dynamic,
             targets: ["FioriThemeManager"]
         )
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/Flight-School/AnyCodable.git", from: "0.2.3"),
-        .package(name: "ObservableArray", url: "https://github.com/sstadelman/observable-array.git", from: "1.2.0"),
-        .package(url: "https://github.com/MarcoEidinger/Zip.git", .upToNextMinor(from: "2.1.2"))
-    ],
+    dependencies: [],
     targets: [
         .target(
             name: "FioriSwiftUI",
-            dependencies: ["FioriSwiftUICore", "FioriIntegrationCards"]
-        ),
-        .target(
-            name: "FioriIntegrationCards",
-            dependencies: ["AnyCodable", "ObservableArray", "FioriCharts", "Zip", "FioriThemeManager"]
+            dependencies: ["FioriSwiftUICore"]
         ),
         .target(
             name: "FioriCharts",
